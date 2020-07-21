3f9e0d4efc07153db2e9548153ce0e5f58a230a8
CAptain_sj <najmulhasansobuj87@gmail.com>
2020-07-21 22:40:32 +0600

    updated

--- inc/header.php
+++ inc/header.php
@@ -6,30 +6,53 @@ header("Expires: Mon, 6 Dec 1977 00:00:00 GMT");
 header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
 ?>
 <!doctype html>
-<html>
-<head>
-	<title>Online Exam System</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<meta http-equiv="Pragma" content="no-cache">
-	<meta http-equiv="no-cache">
-	<meta http-equiv="Expires" content="-1">
-	<meta http-equiv="Cache-Control" content="no-cache">
-	<link rel="stylesheet" href="css/main.css">
-	<script src="js/jquery.js"></script>
-	<script src="js/main.js"></script>
-</head>
-<body>
+<html lang="en">
 
-<div class="phpcoding">
-	<section class="headeroption"></section>
-		<section class="maincontent">
-		<div class="menu">
-		<ul>
-			<li><a href="index.php">Login</a></li>
-			<li><a href="profile.php">Profile</a></li>
-			<li><a href="exam.php">Exam</a></li>
-			<li><a href="register.php">Register</a></li>
-			<li><a href="logout.php">Logout</a></li>
-		</ul>
-		</div>
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+
+    <title>Hello, world!</title>
+</head>
+
+<body>
+    <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-light bc fixed-top">
+            <a class="navbar-brand" href="index.html">CAptainsj</a>
+            <button class="navbar-toggler" data-target="#my-nav" data-toggle="collapse" aria-controls="my-nav"
+                aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div id="my-nav" class="collapse navbar-collapse justify-content-end">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link text-center text-white active" href="login.html">Login<span
+                                class="sr-only"></span></a>
+                    </li>
+                    <li class="nav-item ">
+                        <a class="nav-link text-center text-white active" href="#">Profile <span
+                                class="sr-only"></span></a>
+                    </li>
+                    <li class="nav-item ">
+                        <a class="nav-link text-center text-white active" href="exam.html">Exam <span
+                                class="sr-only"></span></a>
+                    </li>
+                    <li class="nav-item ">
+                        <a class="nav-link text-center text-white active" href="reg.html">Register <span
+                                class="sr-only"></span></a>
+                    </li>
+                    <li class="nav-item ">
+                        <a class="nav-link text-center text-white active" href="#">Logout <span
+                                class="sr-only"></span></a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
 	
