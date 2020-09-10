010f36997d4b7d37cf1c34b184beb0ac9be69eee
Najmul-Hasan-Sobuj <najmulhasansobuj87@gmail.com>
2020-09-10 15:04:22 +0600

    updated

--- register.php
+++ register.php
@@ -1,4 +1,4 @@
-<?php include 'header.php'; ?>
+<?php include 'inc/header.php'; ?>
 
     <div class="section1">
         <div class="title pt-5 text-center">
@@ -8,26 +8,28 @@
             <div class="row">
                 <div class="col-md-3"></div>
                 <div class="col-md-6">
-                    <form>
+                    <form action="" method="post">
                         <div class="form-group pt-5">
-                            <label for="exampleInputname">Name</label>
-                            <input type="text" class="form-control" id="exampleInputname">
+                            <label for="name">Name</label>
+                            <input type="text" class="form-control" id="name" name="name">
                         </div>
                         <div class="form-group">
-                            <label for="exampleInputusername">Username</label>
-                            <input type="text" class="form-control" id="exampleInputusername">
+                            <label for="username">Username</label>
+                            <input type="text" class="form-control" id="username" name="username">
                         </div>
                         <div class="form-group">
-                            <label for="exampleInputPassword1">Password</label>
-                            <input type="password" class="form-control" id="exampleInputPassword1">
+                            <label for="password">Password</label>
+                            <input type="password" class="form-control" id="password" name="password">
                         </div>
                         <div class="form-group">
-                            <label for="exampleInputEmail1">Email address</label>
-                            <input type="email" class="form-control" id="exampleInputEmail1"
+                            <label for="email">Email address</label>
+                            <input type="email" class="form-control" id="email1" name="email"
                                 aria-describedby="emailHelp">
                         </div>
-                        <button type="submit" class="btn btn-primary float-right">Sign up</button>
+                        <button type="submit" id="regsubmit" value="Signup" class="btn btn-primary float-right">Sign up</button>
                     </form>
+			<p>Already Registered ? <a href="index.php">Login</a> Here</p>
+				 <span id="state"></span>
                 </div>
                 <div class="col-md-3"></div>
             </div>
@@ -35,19 +37,4 @@
     </div>
 
 
-
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
-        crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
-        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
-        crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
-        integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI"
-        crossorigin="anonymous"></script>
-</body>
-
-</html>
+<?php include 'inc/footer.php'; ?>
